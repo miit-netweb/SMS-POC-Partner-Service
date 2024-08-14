20031ff9d75bc30dace75214374382fb1152fa62
Abinash Patra <abinash.patra@nwbrd.com>
2024-08-14 19:46:42 +0530

    logger added in handler class

--- Partner-Service/src/main/java/com/microservices/partner/handler/GlobalExceptionHandler.java
+++ Partner-Service/src/main/java/com/microservices/partner/handler/GlobalExceptionHandler.java
@@ -2,6 +2,8 @@ package com.microservices.partner.handler;
 
 import java.time.LocalTime;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -14,6 +16,8 @@ import com.microservices.partner.exception.ValidationException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+	
+	Logger logger=LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
 	@ExceptionHandler(ValidationException.class)
 	public ResponseEntity<ExceptionResponse> handleValidationException(ValidationException exception) {
@@ -23,6 +27,7 @@ public class GlobalExceptionHandler {
 		response.setMessage(exception.getMessage());
 		response.setStatus(exception.getStatus());
 		response.setTimestamp(LocalTime.now().toString());
+		logger.error("ValidationException Response {}",response);
 		return new ResponseEntity<>(response,HttpStatus.BAD_REQUEST);
 	}
 	
@@ -34,6 +39,7 @@ public class GlobalExceptionHandler {
 		response.setMessage(exception.getMessage());
 		response.setStatus(exception.getStatus());
 		response.setTimestamp(LocalTime.now().toString());
+		logger.error("SubTypeNotFoundException Response {}",response);
 		return new ResponseEntity<>(response,HttpStatus.BAD_REQUEST);
 	}
 	
@@ -45,6 +51,7 @@ public class GlobalExceptionHandler {
 		response.setMessage(ex.getMessage());
 		response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR);
 		response.setTimestamp(LocalTime.now().toString());
+		logger.error("GenericException Response {}",response);
 		return new ResponseEntity<>(response,HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 }
